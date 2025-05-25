e18bc2b7515a20702b041b901f392bdb97a1462b
Pippah Gift <Pippah124@gmail.com>
2025-05-25 14:08:22 -0700

    second commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add = number1 + number2 
+sub = number1 - number2
+multiply = number1 * number2
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
