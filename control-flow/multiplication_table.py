d1dfaebbc584354e2415fe42e84aab4422729ff9
Pippah Gift <Pippah124@gmail.com>
2025-06-01 07:00:26 -0700

    corrections

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
-
-for multiplier in range(1, 10):
+for multiplier in range(1, 11):
     result = number * multiplier
     print(f"{number} * {multiplier} = {result}")
