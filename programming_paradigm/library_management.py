3409cd20cbe00f1a72d87e41f851538ff7fbcedc
Pippah Gift <Pippah124@gmail.com>
2025-08-26 05:29:12 -0700

    alx

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -12,6 +12,15 @@ class Library:
 
     def add_book(self,book):
         self._books.append(book)
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book._is_checked_out = False
+                print(f"You have returned '{title}'")
+            return
+    print(f"'{title}' was not checked out or does not exist in the library.")
+
     
     def check_out_book(self, title):
         for book in self._books:
