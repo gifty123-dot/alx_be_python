e9e025f79c60f5ce3ee225e8631530e6d0a0019c
Pippah Gift <Pippah124@gmail.com>
2025-08-26 05:15:18 -0700

    alx

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,6 +8,9 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
 
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+
     def test_divide_by_zero(self):
         self.assertIsNone(self.calc.divide(10, 0))
 
