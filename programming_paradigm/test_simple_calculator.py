9c2dbb369828c3f012ea7dc002f2f207f4962d0c
Pippah Gift <Pippah124@gmail.com>
2025-08-26 05:10:25 -0700

    alx

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(10, 0))
+
+if __name__ == '__main__':
+    unittest.main()
